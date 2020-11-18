e5ca2256aa43333f0eaef777f7dfcd7c35f09b3e
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-11-18 12:26:24 +0900

    編集更新途中

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -43,10 +43,9 @@ class TodoController extends Controller
         $todo = new Todo();
         $todo->title = $request->title;
         $todo->due_date = $request->due_date;
-        $todo->status = Todo::STATUS_NOT_YET;
+        todo()->save();
 
-        Auth::user()->todos()->save($todo);
-        return redirect()->to('/todo');
+        return redirect()->to('/todo',$todo->id);
     }
 
     /**
@@ -80,9 +79,15 @@ class TodoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(CreateTodoRequest $request, $id)
     {
-        //
+        //フォームから送信されたタスクを書き換える
+        $todo = Auth::user()->todos()->findOrFail($id);
+        $todo->title = $request->title;
+        $todo->due_date = $request->due_date;
+        $todo->status = Todo::STATUS_NOT_YET;
+        Auth::user()->todos()->save($todo);
+        return redirect()->to('/todo');
     }
 
     /**
