846b5de34140ba695210b27fefe67fa0fcbbed0e
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-12-09 12:28:12 +0900

    編集からの削除機能追加

--- resources/views/todo/edit.blade.php
+++ resources/views/todo/edit.blade.php
@@ -3,7 +3,17 @@
 @section('content')
     <div class="container">
         <div class="col-md-10">
-            <h2 class="text-muted py-3">Todo 編集</h2>
+            <h2 class="text-muted py-3">
+                Todo 編集
+                <div class="float-right my-3">
+                    <form action="/todo/{{ $todo->id }}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <button class="btn btn-danger" type="submit"><i class="fas fa-trash-alt mr-2"></i>削除</button>
+                    </form>
+                </div>
+
+            </h2>
             <form action="/todo/{{ $todo->id }}" method="POST">
                 @method('PUT')
                 @include('parts.todo.input', ['todo' => $todo])
