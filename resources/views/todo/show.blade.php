c9da55c980f419187fe1478406288faa06797db5
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-12-16 09:45:45 +0900

    個別のタスクからの編集画面への遷移と削除機能の追加。削除に関しては編集からも可能。

--- resources/views/todo/show.blade.php
+++ resources/views/todo/show.blade.php
@@ -3,6 +3,13 @@
     <div class="container">
         <div class="col-md-10">
         <h2 class="text-muted py-3">{{ $todo->title }}</h2>
+            <div class="float-right my-3">
+                <form action="/todo/{{ $todo->id }}" method="POST">
+                    @method('DELETE')
+                    @csrf
+                    <button class="btn btn-danger" type="submit"><i class="fas fa-trash-alt mr-2"></i>削除</button>
+                </form>
+            </div>
         <table class="table">
             <thead>
             <tr>
@@ -19,6 +26,11 @@
             </tr>
             </tbody>
         </table>
+        <div>
+        <th>
+            <a href="/todo/{{ $todo->id }}/edit" class="btn btn-success"><i class="fas fa-edit mr-2"></i>編集</a>
+        </th>
+        <hr>        
         @include('parts.button.back')
     </div>
 @endsection
